7e2f62392ebefbe9238908b8406a7fecd5751340
lexytaylormb <141174772+heacol2230@users.noreply.github.com>
2024-05-15 12:08:50 -0400

    Initial setup of files for web app

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#000033',
+        sky: '#7cd8e0',
+        pink: '#ec65e8',
+        white: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+}
+
